36aa9e2e0a6a23e07e6b784a59ba2ef3717fd3ec
Chu Minh <chuminh712@gmail.com>
2020-08-07 17:59:26 +0700

    Static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def home; end
+
+  def about_us; end
+
+  def contact_us; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+
+    get "/about-us", to: "static_pages#about_us"
+    get "/contact-us", to: "static_pages#contact_us"
+  end
 end
